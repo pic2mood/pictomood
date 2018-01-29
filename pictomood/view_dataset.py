2f4b78d6f445299cb812cd759aa5f19a95c643fd
raymelon <franciscoraymel@gmail.com>
2018-01-29 22:18:46 +0800

    view_dataset tweak

--- pictomood/view_dataset.py
+++ pictomood/view_dataset.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     #     if sys.argv[1]:
     #         pictomood.utils.view_dataset(sys.argv[1])
 
-    # pictomood.utils.view_dataset(config.trainer_oea)
+    pictomood.utils.view_dataset(config.trainer_oea)
     pictomood.utils.plot_dataset(config.trainer_oea)
